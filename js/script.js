9e15a901151f0bcff01b299c2580d5396b11187a
Douglas Camata <douglas.camata@gmail.com>
2023-11-24 13:51:37 -0300

    Atualizacao

--- js/script.js
+++ js/script.js
@@ -5,28 +5,45 @@ function carregarAlunos() {
         })
         .then(function (data) {
             for (let i = 0; i < data.length; i++) {
-                                
+
                 var cardAluno = ""
                 cardAluno += `
                     <div class="card-aluno">
                         <img src=${data[i].foto}>
-                        <h2>${data[i].nome}</h2>     
+                        <h2>${data[i].nome}</h2>  
+                        <hr>   
                         <p>Projetos 1º Sem.</p>
                         <div class="botoes">
-                            <div class="blur"></div>
+                            <div class="blur"></div>                            
                             `
-
-                for (let x = 0; x < data[i].trabalhos.length; x++){
+                for (let x = 0; x < data[i].trabalhos.length; x++) {
                     cardAluno += `
                         <a href="${data[i].trabalhos[x].link}" target="_blank" class="botao">${data[i].trabalhos[x].projeto}</a>
                         `
                 }
 
-                cardAluno += `
+
+                if (data[i].trabalhos2sem != undefined) {
+                    cardAluno += `
+                        </div>       
+
+                        <p>Projetos 2º Sem.</p>
+                        <div class="botoes">
+                            <div class="blur"></div>                            
+                            `
+
+                    for (let x = 0; x < data[i].trabalhos2sem.length; x++) {
+                        cardAluno += `
+                        <a href="${data[i].trabalhos2sem[x].link}" target="_blank" class="botao">${data[i].trabalhos2sem[x].projeto}</a>
+                        `
+                    }
+
+                    cardAluno += `
                         </div>                 
                     </div>
                     `
-                
+                }
+
                 var lista = document.getElementById("alunos")
                 lista.innerHTML += cardAluno
             }
@@ -38,28 +55,46 @@ function carregarAlunos() {
         })
         .then(function (data) {
             for (let i = 0; i < data.length; i++) {
-                                
+
                 var cardAluno = ""
                 cardAluno += `
                     <div class="card-aluno">
                         <img src=${data[i].foto}>
-                        <h2>${data[i].nome}</h2>     
+                        <h2>${data[i].nome}</h2>    
+                        <hr> 
                         <p>Projetos 1º Sem.</p>
                         <div class="botoes">
-                            <div class="blur"></div>
-                            
+                            <div class="blur"></div>                            
                             `
 
-                for (let x = 0; x < data[i].trabalhos.length; x++){
+                for (let x = 0; x < data[i].trabalhos.length; x++) {
                     cardAluno += `
                         <a href="${data[i].trabalhos[x].link}" target="_blank" class="botao">${data[i].trabalhos[x].projeto}</a>
                         `
                 }
 
-                cardAluno += `
+
+                if (data[i].trabalhos2sem != undefined) {
+                    cardAluno += `
+                        </div>      
+
+                        <p>Projetos 2º Sem.</p>
+                        <div class="botoes">
+                            <div class="blur"></div>                            
+                            `
+
+                    for (let x = 0; x < data[i].trabalhos2sem.length; x++) {
+                        cardAluno += `
+                        <a href="${data[i].trabalhos2sem[x].link}" target="_blank" class="botao">${data[i].trabalhos2sem[x].projeto}</a>
+                        `
+                    }
+
+                    cardAluno += `
                         </div>                 
                     </div>
                     `
+                }
+
                 var lista = document.getElementById("instrutores")
                 lista.innerHTML += cardAluno
             }
